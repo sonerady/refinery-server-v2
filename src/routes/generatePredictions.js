83f2636c9ff8b04bab8b16b5d9ff2c0fa1ea4b4c
Soner <80179760+sonerady@users.noreply.github.com>
2024-11-16 04:10:20 +0300

    fixed

--- src/routes/generatePredictions.js
+++ src/routes/generatePredictions.js
@@ -158,7 +158,7 @@ async function generateImagesWithReplicate(
         input: {
           prompt: modifiedPrompt,
           hf_loras: combinedHfLoras,
-          lora_scales: [0.85],
+          lora_scales: [0.95],
           num_outputs: imageCount,
           aspect_ratio: imageRatio,
           output_format: imageFormat,
